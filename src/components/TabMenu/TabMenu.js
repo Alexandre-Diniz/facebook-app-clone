01704d7a229341968d8bc96bc6437dbfd4483cd4
MirLih <alexandre.diniz.dias@gmail.com>
2020-07-19 16:29:40 -0300

    microline

--- src/components/TabMenu/TabMenu.js
+++ src/components/TabMenu/TabMenu.js
@@ -19,52 +19,62 @@ export default function () {
       width: '100%',
       height: 50,
       top: top,
-      flexDirection: 'row',
       position: 'absolute',
-      backgroundColor:'#FFF'
+      backgroundColor: '#FFF'
     }} >
-      <Home
-        setFocused={focused => setFocused(focused)}
-        focused={focused}
-        setTop={top => {
-          console.log(top)
-          setTop(top)
-        }} />
-      <Group
-        setFocused={focused => setFocused(focused)}
-        focused={focused}
-        setTop={top => {
-          console.log(top)
-          setTop(top)
-        }} />
-      <Live
-        setFocused={focused => setFocused(focused)}
-        focused={focused}
-        setTop={top => {
-          console.log(top)
-          setTop(top)
-        }} />
-      <Games
-        setFocused={focused => setFocused(focused)}
-        focused={focused}
-        setTop={top => {
-          console.log(top)
-          setTop(top)
-        }} />
-      <Notificatins
-        setFocused={focused => setFocused(focused)}
-        focused={focused}
-        setTop={top => {
-          console.log(top)
-          setTop(top)
-        }} />
-      <Menu
-        setFocused={focused => setFocused(focused)}
-        focused={focused}
-        setTop={top => {
-          console.log(top)
-          setTop(top)
-        }} />
+      <View
+        style={{
+          width: '100%',
+          height: '100%',
+          flexDirection:'row'
+        }}
+      >
+        <Home
+          setFocused={focused => setFocused(focused)}
+          focused={focused}
+          setTop={top => {
+            console.log(top)
+            setTop(top)
+          }} />
+        <Group
+          setFocused={focused => setFocused(focused)}
+          focused={focused}
+          setTop={top => {
+            console.log(top)
+            setTop(top)
+          }} />
+        <Live
+          setFocused={focused => setFocused(focused)}
+          focused={focused}
+          setTop={top => {
+            console.log(top)
+            setTop(top)
+          }} />
+        <Games
+          setFocused={focused => setFocused(focused)}
+          focused={focused}
+          setTop={top => {
+            console.log(top)
+            setTop(top)
+          }} />
+        <Notificatins
+          setFocused={focused => setFocused(focused)}
+          focused={focused}
+          setTop={top => {
+            console.log(top)
+            setTop(top)
+          }} />
+        <Menu
+          setFocused={focused => setFocused(focused)}
+          focused={focused}
+          setTop={top => {
+            console.log(top)
+            setTop(top)
+          }} />
+      </View>
+
+      <View style={{ width: '100%', height: 0.2, backgroundColor: '#65666A' }} />
+
     </Animated.View>
   )
 }
